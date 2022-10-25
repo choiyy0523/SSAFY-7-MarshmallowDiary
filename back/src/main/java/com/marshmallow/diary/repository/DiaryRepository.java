1ec72dc44a64990f4b2475d4beb1fb81af9b6eb0
김현지 <sionbu4608@naver.com>
2022-10-25 14:56:07 +0900

    feat : s3 서버 세팅, diary 등록, 조회 기능 구현

--- back/src/main/java/com/marshmallow/diary/repository/DiaryRepository.java
+++ back/src/main/java/com/marshmallow/diary/repository/DiaryRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface DiaryRpository extends JpaRepository<Diary, UUID> {
+public interface DiaryRepository extends JpaRepository<Diary, UUID> {
 }
 
