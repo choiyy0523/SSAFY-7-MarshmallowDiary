7d10b7c5567d35e1fc90523de23810256c52014c
Yujiseul <seul_ll@naver.com>
2022-11-01 12:56:05 +0900

    [#96] feat : swagger api 추가

--- back/src/main/java/com/marshmallow/analysis/controller/AnalysisController.java
+++ back/src/main/java/com/marshmallow/analysis/controller/AnalysisController.java
@@ -3,6 +3,7 @@ package com.marshmallow.analysis.controller;
 import com.marshmallow.analysis.dto.AnalysisRequest;
 import com.marshmallow.analysis.dto.AnalysisResponse;
 import com.marshmallow.analysis.service.AnalysisService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/analysis")
+@ApiOperation(value = "[분석] Analysis Controller")
 public class AnalysisController {
 
     @Autowired
@@ -30,6 +32,7 @@ public class AnalysisController {
 //    }
 
     @PostMapping("/{diaryId}}")
+    @ApiOperation(value = "해당 일기의 감정 분석 결과", notes = "다이어리 아이디를 입력받아 해당 일기의 감정 분석 결과 조회 기능")
     public ResponseEntity getEmotion(@PathVariable UUID diaryId, HttpServletRequest res) throws Exception {
 
         AnalysisResponse.getResult result = analysisService.result(diaryId);
@@ -38,6 +41,7 @@ public class AnalysisController {
     }
 
     @GetMapping("/loyalty")
+    @ApiOperation(value = "메인페이지 마로,별로,시로 값과 멘트 전달", notes = "얼마나 일기를 꾸준하게 작성했는지 판단하는 기능")
     ResponseEntity<Map<Integer, String>> showMain(){
         Map<Integer, String> result = analysisService.main();
 
@@ -45,6 +49,7 @@ public class AnalysisController {
     }
 
     @GetMapping("/positive")
+    @ApiOperation(value = "긍정 일기 조회", notes = "긍정적인 일기를 조회하는 기능")
     ResponseEntity getId() throws Exception {
         AnalysisResponse.getdiaryId result = analysisService.getdiaryId();
 
@@ -52,6 +57,7 @@ public class AnalysisController {
     }
 
     @GetMapping("/all")
+    @ApiOperation(value = "전체 기간 동안 감정 분석 결과 조회", notes = "전체 기간동안 긍정, 중립, 부장의 값의 합을 제공")
     ResponseEntity getAllEmotion() {
         AnalysisResponse.getAllEmotion result = analysisService.getAllEmotion();
 
@@ -59,6 +65,7 @@ public class AnalysisController {
     }
 
     @GetMapping("/month")
+    @ApiOperation(value = "해당 월의 감정 분석 결과 조회", notes = "해당 월의 긍정, 중립, 부장의 값의 합을 제공")
     ResponseEntity getMonthEmotion(@RequestBody AnalysisRequest.monthReq req) throws ParseException {
         AnalysisResponse.getAllEmotion result = analysisService.getMonthEmotion(req.getYear(), req.getMonth());
         return ResponseEntity.status(200).body(result);
