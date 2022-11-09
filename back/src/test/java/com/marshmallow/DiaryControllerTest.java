f574e04cec282570d7cbf567ad2e0e293088159c
이도연 <dyl1229@naver.com>
2022-11-09 10:41:26 +0900

    [#S07P31A303-80] feat: 테스트 설정 및 일기 작성 테스트

--- back/src/test/java/com/marshmallow/DiaryControllerTest.java
+++ back/src/test/java/com/marshmallow/DiaryControllerTest.java
@@ -1,8 +1,10 @@
 package com.marshmallow;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marshmallow.diary.controller.DiaryController;
 import com.marshmallow.diary.service.DiaryService;
+import lombok.*;
 import org.junit.jupiter.api.BeforeEach; //not junit JUPITER!
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,14 +20,18 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc
+@Transactional
 public class DiaryControllerTest {
 
     @InjectMocks
@@ -38,113 +44,112 @@ public class DiaryControllerTest {
     // HTTP 호출을 위한 객체
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLsnbTrj4Tsl7AiLCJpYXQiOjE2Njc4OTczODUsImV4cCI6MTY3MDMxNjU4NX0.ipmGbA3lUrkLVmtnxzjsTeOvXiBoM6xKBqbRZZjUSM8";
+
     @BeforeEach
-    public void init(){
+    public void init() {
 //        mockMvc = MockMvcBuilders
 //                .standaloneSetup(diaryController) // 테스트 대상 Controller 를 넣어준다.
 //                .alwaysExpect(MockMvcResultMatchers.status().isOk()) // 특정 필수 조건을 지정
 //                .build();
     }
 
-//    public static class Diary {
-//        private String title = "피곤해";
-//        private String content = "너무너무 피곤하다";
-//        private int weather = "1";
-//        @JsonFormat(pattern = "yyyy-MM-dd" )
-//        private Date date;
-//    }
+    @Builder
+    @Getter
+    public static class TestDiary{
+        private String title;
+        private String content;
+        private int weather;
+        @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+9")
+        private Date date;
+    }
 
     /***********************다이어리 조회***********************/
     @Test
     @DisplayName("특정 일에 작성한 일기 조회")
     @Rollback
     public void detailsDiary_Exist() throws Exception {
-
-
-
         mockMvc.perform(
                 MockMvcRequestBuilders
                         .get("/diary/detail/{date}", "2022-11-09")
-                        .header(HttpHeaders.AUTHORIZATION, "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLsnbTrj4Tsl7AiLCJpYXQiOjE2Njc4OTczODUsImV4cCI6MTY3MDMxNjU4NX0.ipmGbA3lUrkLVmtnxzjsTeOvXiBoM6xKBqbRZZjUSM8")
-//                        .accept(MediaType.APPLICATION_JSON)
-        )
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 날짜의 일기 조회")
+    @Rollback
+    public void detailsDiary_NotExist() throws Exception {
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/diary/detail/{date}", "2022-11-10")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isNotFound());
+    }
+
+    /***********************다이어리 등록***********************/
+    @Test
+    @DisplayName("일기 작성-텍스트")
+    @Rollback
+    public void createDiary_Text() throws Exception {
+        String dateString = "2000-01-03";
+        SimpleDateFormat dtFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = dtFormat.parse(dateString);
+
+        String title = "배가고파요";
+        String content = "오늘 밥 뭐더라";
+        int weather = 1;
+
+        TestDiary inputDiary = TestDiary.builder()
+                .title(title)
+                .content(content)
+                .weather(weather)
+                .date(date)
+                .build();
+        String requestContent = objectMapper.writeValueAsString(inputDiary);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .post("/diary/regist/diary")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                                .content(requestContent)
+                                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
                 .andExpect(status().isOk());
 
-
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/diary/detail/{date}", dateString)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("title").value(title))
+                .andExpect(jsonPath("content").value(content))
+                .andExpect(jsonPath("date").value(dateString))
+                .andExpect(jsonPath("weather").value(Integer.toString(weather)));
     }
 
     @Test
-    @DisplayName("특정 일에 작성한 일기 조회")
+    @DisplayName("일기 작성-사진 한장")
     @Rollback
-    public void detailsDiary_NotExist(){
-        // given
+    public void createDiary_Picture(){
 
-
-        // when
-
-
-        // then
-
-
-    }
-    
-    
-    /***********************다이어리 등록***********************/
-    
-    
-    @Test
-    @DisplayName("일기 작성-빈 내용")
-    @Rollback
-    public void createDiary_EmptyContent(){
-        // given
-
-
-        // when
-
-
-        // then
 
 
     }
 
     @Test
-    @DisplayName("일기 작성-텍스트만")
+    @DisplayName("일기 작성-사진 여러장")
     @Rollback
-    public void createDiary_Text(){
-        // given
+    public void createDiary_Pictures(){
 
 
-        // when
 
-
-        // then
-
-    }
-
-    @Test
-    @DisplayName("일기 작성-텍스트와 사진")
-    @Rollback
-    public void createDiary_TextAndPicture(){
-        // given
-
-
-        // when
-
-
-        // then
-    }
-
-    @Test
-    @DisplayName("일기 작성-텍스트와 사진 여러장")
-    @Rollback
-    public void createDiary_TextAndPictures(){
-        // given
-
-
-        // when
-
-
-        // then
     }
 
 
