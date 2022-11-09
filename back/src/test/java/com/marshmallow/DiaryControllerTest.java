20c7d28f8df51926276c26e19b75f3ef03841bca
이도연 <dyl1229@naver.com>
2022-11-09 14:57:36 +0900

    [#S07P31A303-80] feat: 다이어리 사진 등록 테스트

--- back/src/test/java/com/marshmallow/DiaryControllerTest.java
+++ back/src/test/java/com/marshmallow/DiaryControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marshmallow.diary.controller.DiaryController;
 import com.marshmallow.diary.service.DiaryService;
 import lombok.*;
+
 import org.junit.jupiter.api.BeforeEach; //not junit JUPITER!
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,13 +16,18 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
+import java.io.*;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -43,9 +49,10 @@ public class DiaryControllerTest {
     @Autowired
     // HTTP 호출을 위한 객체
     private MockMvc mockMvc;
-
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    ResourceLoader loader;
 
     private final String accessToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiLsnbTrj4Tsl7AiLCJpYXQiOjE2Njc4OTczODUsImV4cCI6MTY3MDMxNjU4NX0.ipmGbA3lUrkLVmtnxzjsTeOvXiBoM6xKBqbRZZjUSM8";
 
@@ -131,27 +138,78 @@ public class DiaryControllerTest {
                 .andExpect(jsonPath("title").value(title))
                 .andExpect(jsonPath("content").value(content))
                 .andExpect(jsonPath("date").value(dateString))
-                .andExpect(jsonPath("weather").value(Integer.toString(weather)));
+                .andExpect(jsonPath("weather").value(Integer.toString(weather)))
+//                .andExpect(jsonPath("photo").value(null))
+        ;
     }
 
     @Test
     @DisplayName("일기 작성-사진 한장")
     @Rollback
-    public void createDiary_Picture(){
+    public void createDiary_Picture() throws Exception {
+        String dateString = "2000-01-02";
+        String curPath = new File("").getAbsolutePath();
+        String filename = "\\src\\test\\images\\jeju.jpg";
 
+        File file = new File(curPath + filename);
+        MockMultipartFile multipartFile = new MockMultipartFile("photos", curPath + filename, "image/jpg", Files.readAllBytes(file.toPath()));
+//        MockMultipartFile multipartFile = new MockMultipartFile("image", new FileInputStream(file));
+//        MockMultipartFile multipartFile = new MockMultipartFile("photos", curPath + filename, "image/jpg", "<<jpg data>>".getBytes());
 
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .multipart("/diary/regist/photo/{date}", dateString)
+                                .file(multipartFile)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk());
 
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/diary/detail/{date}", dateString)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("title").value("테스트 제목"))
+                .andExpect(jsonPath("content").value("테스트 내용"))
+                .andExpect(jsonPath("photo").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.photo.length()").value(1));
     }
 
     @Test
     @DisplayName("일기 작성-사진 여러장")
     @Rollback
-    public void createDiary_Pictures(){
+    public void createDiary_Pictures() throws Exception {
+        String dateString = "2000-01-02";
+        String curPath = new File("").getAbsolutePath();
+        String filename1 = "\\src\\test\\images\\jeju.jpg";
+        String filename2 = "\\src\\test\\images\\profile.jpg";
 
+        File file1 = new File(curPath + filename1);
+        File file2 = new File(curPath + filename2);
+        MockMultipartFile multipartFile1 = new MockMultipartFile("photos", curPath + filename1, "image/jpg", Files.readAllBytes(file1.toPath()));
+        MockMultipartFile multipartFile2 = new MockMultipartFile("photos", curPath + filename2, "image/jpg", Files.readAllBytes(file2.toPath()));
 
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .multipart("/diary/regist/photo/{date}", dateString)
+                                .file(multipartFile1)
+                                .file(multipartFile2)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk());
 
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/diary/detail/{date}", dateString)
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("title").value("테스트 제목"))
+                .andExpect(jsonPath("content").value("테스트 내용"))
+                .andExpect(jsonPath("photo").isNotEmpty())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.photo.length()").value(2));
     }
-
 
     /***********************다이어리 삭제***********************/
 
