2879f026726fafbadd1425782d0a9769eed10821
최윤영 <adokids2@gmail.com>
2022-11-11 16:49:07 +0900

    fix : 빌드 전 구현 준비중 기능 주석 처리

--- MarshmallowDiary/src/screens/pages/Settings/OpenSource.js
+++ MarshmallowDiary/src/screens/pages/Settings/OpenSource.js
@@ -1,30 +1,65 @@
 import React from 'react';
-import { FlatList, ListRenderItem, Text, View } from 'react-native';
+import { FlatList, ListRenderItem, Text, View, StyleSheet } from 'react-native';
 import MD_OS_FE from './MD_OS_FE'
 import { MarshmallowDiaryOpenSourceLicenseBE } from './MarshmallowDiaryOpenSourceLicenseBE.json'
+// import Hyperlink from 'react-native-hyperlink'
+// import openURL from '../../components/component/openUrl';
 
-const renderItem = ({ item }) => {
-  return (
-    <>
-      <View>
-        <Text>{item.libraryName}</Text>
-        <Text>{item.version}</Text>
-        <Text>{item._license}</Text>
-        <Text>{item._description}</Text>
-      </View>
-    </>
-  );
-}
+// const renderItem = ({ item }) => {
+//   return (
+//     <>
+//       <View>
+//         <Text>{item.libraryName}</Text>
+//         <Text>{item.version}</Text>
+//         <Text>{item._license}</Text>
+//         <Text>{item._description}</Text>
+//       </View>
+//     </>
+//   );
+// }
+
+// const OpenSource = () => {
+//   return (
+//     <View>
+//       <FlatList
+//         data={MD_OS_FE}
+//         renderItem={renderItem}
+//         keyExtractor={(item) => String(item.libraryName)}
+//       />
+//     </View>
+//   )
+// };
+
+
+// function OpenSource() {
+//   const contents = '오픈소스 라이센스 : https://marshmallowdiary.notion.site/1a6a1966970742c7bc1940d7db4144d5'
+//   return (
+//     <Hyperlink
+//       linkStyle={styles.hyperlinkStyle}
+//       onPress={(url) => openURL(url)}
+//     >
+//       <Text style={styles.contentStyle}>{contents}</Text>
+//     </Hyperlink>
+//   )
+// }
+
+// const styles = StyleSheet.create({
+//   hyperlinkStyle: {
+//     fontSize: 16,
+//     color: '#505050'
+//   },
+//   contentStyle: {
+//     fontSize: 18,
+//     color: '#111111'
+//   }
+// })
+
 
 const OpenSource = () => {
   return (
     <View>
-      <FlatList
-        data={MD_OS_FE}
-        renderItem={renderItem}
-        keyExtractor={(item) => String(item.libraryName)}
-      />
+      <Text> 오픈소스 라이센스 : https://marshmallowdiary.notion.site/1a6a1966970742c7bc1940d7db4144d5 </Text>
     </View>
   )
 };
-export default OpenSource;
+export default OpenSource
