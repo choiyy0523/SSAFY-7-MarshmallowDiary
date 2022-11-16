97a1e4a23a66ecfed600ed8b7d0c07fd80386758
최윤영 <adokids2@gmail.com>
2022-11-16 14:01:33 +0900

    feat : 폰트 변경, env 변경, merge

--- MarshmallowDiary/src/api/http.js
+++ MarshmallowDiary/src/api/http.js
@@ -5,7 +5,8 @@ import Config from "react-native-config";
 // 4주 이상 미 접속시 access 만료, axios 요청 시 권한 없으면 로그인 유도해서 재발급
 
 const axiosInstance = axios.create({
-  baseURL: Config.API_BASE_URL,
+  // baseURL: Config.API_BASE_URL,
+  baseURL: "http://k7a303.p.ssafy.io:9090/api/v1",
   headers: {
     "Content-type": "application/json",
   },
@@ -14,7 +15,7 @@ const axiosInstance = axios.create({
 console.log(Config.REACT_APP_API_BASE_URL)
 
 axiosInstance.interceptors.request.use(
-  async(config : any) => {
+  async (config: any) => {
     const token = await AsyncStorage.getItem('token')
     config.headers["Content-Type"] = "application/json; charset=utf-8"
     config.headers["Authorization"] = `Bearer ${token}`
